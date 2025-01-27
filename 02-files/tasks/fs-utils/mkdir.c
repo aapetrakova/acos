5e9c8d2dc875f4530957b97df2027fa2879a813b
aapetrakova <petrakova.aa@phystech.edu>
2025-01-27 14:26:47 +0300

    fs-utils

--- 02-files/tasks/fs-utils/mkdir.c
+++ 02-files/tasks/fs-utils/mkdir.c
@@ -1,4 +1,65 @@
-int main() {
-    // TODO: implement me
-    return 0;
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+    
+int create_directory(const char* path, mode_t mode) {
+  struct stat dir_info;
+  if (stat(path, &dir_info) == 0) {
+    if (S_ISDIR(dir_info.st_mode)) { return 0; } 
+    else { errno = ENOTDIR; return -1; }
+  }
+
+  if (mkdir(path, mode) != 0 && errno != EEXIST) { return -1; }
+
+  return 0;
+}
+
+int create_directory_with_parents(const char* path, mode_t mode) {
+  char* temp_path = (char*)malloc(strlen(path) * sizeof(char) + 1);
+  if (!temp_path) { return -1; }
+  strcpy(temp_path, path);
+  char* pos = temp_path;
+  
+  int result = 0;
+  while ((pos = strchr(pos, '/')) != NULL) {
+    if (temp_path != pos) {
+      *pos = '\0';
+      if ((result = create_directory(temp_path, 0775)) != 0) { break; }
+      *pos = '/';
+    }
+    ++pos;
+  }
+  
+  free(temp_path);
+  
+  return (result ? result : create_directory(path, mode));
+}
+  
+int main(int argc, char* argv[]) {
+  int opt;
+  int err = 0;
+  _Bool parent_dirs = 0;
+  mode_t mode = 0775;
+
+  const char* short_options = "m:p";
+  const struct option long_options[] = {
+    {"mode", required_argument, NULL, 'm'},
+    {NULL, 0, NULL, 0}
+  };
+
+  while ((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+    if (opt == 'p') { parent_dirs = 1; }
+    else if (opt == 'm') { mode = strtol(optarg, NULL, 8) & 0777; }
+    else { return opterr; }
+  }
+
+  for (char** dir = argv + optind; dir < argv + argc; ++dir) {
+    if (parent_dirs) { err = create_directory_with_parents(*dir, mode); }
+    else { err = create_directory(*dir, mode); }
+  }
+  return err;
 }
--- 02-files/tasks/fs-utils/rm.c
+++ 02-files/tasks/fs-utils/rm.c
@@ -1,4 +1,63 @@
-int main() {
-    // TODO: implement me
-    return 0;
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int remove_path(const char* path, _Bool recursive_mode) {
+  struct stat path_info;
+  if (lstat(path, &path_info) == -1) { return -1; }
+
+  if (S_ISDIR(path_info.st_mode)) {
+    if (!recursive_mode) {
+      errno = EISDIR;
+      return -1;
+    }
+
+    DIR* dir_stream = opendir(path);
+    if (dir_stream == NULL) { return -1; }
+
+    struct dirent* entry;
+    while ((entry = readdir(dir_stream)) != NULL) {
+      if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+        char* new_full_path = malloc(strlen(path) + strlen(entry->d_name) + 2);
+        if (new_full_path == NULL) {
+            closedir(dir_stream);
+            return -1;
+        }
+
+        sprintf(new_full_path, "%s/%s", path, entry->d_name);
+
+        if (remove_path(new_full_path, 1) == -1) {
+          free(new_full_path);
+          closedir(dir_stream);
+          return -1;
+        }
+        free(new_full_path);
+      }
+    }
+
+    closedir(dir_stream);
+    return rmdir(path);
+  } else { return unlink(path); }
+}
+
+int remove_multiple_paths(char* paths[], size_t num_paths, _Bool recursive_mode) {
+  for (size_t i = 0; i < num_paths; ++i) {
+    if (remove_path(paths[i], recursive_mode) < 0) { return -1; }
+  }
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  _Bool recursive_mode = 0;
+  int opt;
+  while ((opt = getopt(argc, argv, "r")) != -1) {
+    if (opt == 'r') { recursive_mode = 1; }
+    else { return opterr; }
+    }
+
+    return remove_multiple_paths(argv + optind, argc - optind, recursive_mode);
 }
