e25fb0226f73516a7f7ad889d92947501f772161
aapetrakova <petrakova.aa@phystech.edu>
2025-01-29 18:49:09 +0300

    tailf

--- 02-files/tasks/tailf/tailf.c
+++ 02-files/tasks/tailf/tailf.c
@@ -1,3 +1,32 @@
-int main() {
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int tail_f(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) { return -1; }
+
+    char buffer[1024];
+    int bytes_read;
+
+    while (1) {
+        bytes_read = read(fd, buffer, sizeof(buffer));
+        if (bytes_read != -1) {
+            if (write(STDOUT_FILENO, buffer, bytes_read) == -1) { return -1; }
+        } else if (bytes_read < 1024) {
+            break;
+        }
+        usleep(1000);
+    }
+
+    close(fd);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) { return -1; }
+
+    tail_f(argv[1]);
+
     return 0;
 }
