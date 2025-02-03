85b2d9068490f5202a9f9aa5702da3cd3c0eed3f
aapetrakova <petrakova.aa@phystech.edu>
2025-02-03 14:37:17 +0300

    brocken-echo

--- 15-bonus/tasks/broken-echo/fix.c
+++ 15-bonus/tasks/broken-echo/fix.c
@@ -1,3 +1,40 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
 void fix_broken_echo() {
-    // TODO: fix broken echo
+  int pid = getppid();
+  char input_file_path[4096];
+  int input = -1;
+
+  size_t descriptor = 3;
+  while (descriptor != 128) {
+    snprintf(input_file_path, 4096, "/proc/%u/fd/%zu", pid, descriptor);
+    input = open(input_file_path, O_RDONLY);
+    descriptor++;
+    if (input != -1) { break; } 
+  }
+
+  if (input == -1) { return; }
+
+  int output = open("/proc/self/fd/1", O_WRONLY);
+  if (output == -1) {
+    close(input);
+    return;
+  }
+
+  char buffer[4096];
+  int read_bytes = read(input, buffer, 4096);
+  if (read_bytes == -1) {
+    close(input);
+    close(output);
+    return;
+  }
+
+  if (write(output, buffer, read_bytes) == -1) {
+    perror("Error writing to output file");
+  }
+  close(input);
+  close(output);
 }
+
