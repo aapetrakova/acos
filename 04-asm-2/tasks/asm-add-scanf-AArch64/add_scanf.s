0b2ba596ef6160c82093d9a85f1816afacc1a7d9
aapetrakova <petrakova.aa@phystech.edu>
2025-02-12 11:04:46 +0300

    asm-add-scanf-AArch64

--- 04-asm-2/tasks/asm-add-scanf-AArch64/add_scanf.s
+++ 04-asm-2/tasks/asm-add-scanf-AArch64/add_scanf.s
@@ -12,7 +12,18 @@
   .endm
 
 add_scanf:
-  // ???
+  push x29
+  mov x29, sp
+  sub sp, sp, 16
+  ldr x0, =scanf_format_string
+  ldr x1, [sp]
+  ldr x2, [sp, 8]
+  bl scanf
+  ldr x0, [sp]
+  ldr x1, [sp, 8]
+  add x0, x0, x1
+  add sp, sp, 16
+  pop x29
 
   ret
 
