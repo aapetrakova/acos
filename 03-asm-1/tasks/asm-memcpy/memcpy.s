94ad0600559da778222f725a270d90679ece3aa8
aapetrakova <petrakova.aa@phystech.edu>
2025-02-12 11:40:47 +0300

    asm-memcpy

--- 03-asm-1/tasks/asm-memcpy/memcpy.s
+++ 03-asm-1/tasks/asm-memcpy/memcpy.s
@@ -4,6 +4,35 @@
   .global my_memcpy
 
 my_memcpy:
-  # ???
+  push rbp
+  mov rbp, rsp
+  push rdi
+  cmp edx, 8
+  jb finish_fast
 
+  fast:
+  sub edx, 8
+  mov rax, [rsi]
+  mov [rdi], rax
+  add rdi, 8
+  add rsi, 8
+  cmp edx, 8
+  jae fast
+
+  finish_fast:
+  test edx, edx
+  jz finish
+
+  slow:
+  dec edx
+  mov al, [rsi]
+  mov [rdi], al
+  add rdi, 1
+  add rsi, 1
+  test edx, edx
+  jnz slow
+
+  finish:
+  pop rax
+  pop rbp
   ret
