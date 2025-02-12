9aea271dabd41f41a76ed6715935a93319695820
aapetrakova <petrakova.aa@phystech.edu>
2025-02-12 10:59:07 +0300

    asm-add-scanf

--- 03-asm-1/tasks/asm-add-scanf/add_scanf.s
+++ 03-asm-1/tasks/asm-add-scanf/add_scanf.s
@@ -4,11 +4,21 @@
   .global add_scanf
 
 add_scanf:
-  # ???
-
+  push rbp
+  mov rbp, rsp
+  sub rsp, 16
+  lea rdi, [scanf_format_string]
+  lea rsi, [rsp]
+  lea rdx, [rsp + 8]
+  call scanf
+  mov rax, [rsp]
+  add rax, [rsp + 8]
+  add rsp, 16
+  pop rbp
   ret
 
   .section .rodata
 
 scanf_format_string:
   .string "%lld %lld"
+
