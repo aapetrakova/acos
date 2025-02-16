f2ab28d350ae4874ee0c8362b73db1ebf55d8087
aapetrakova <petrakova.aa@phystech.edu>
2025-02-16 16:37:41 +0300

    asm-1d-dp vers2

--- 04-asm-2/tasks/asm-1d-dp/longest_inc_subseq.s
+++ 04-asm-2/tasks/asm-1d-dp/longest_inc_subseq.s
@@ -1,56 +1,53 @@
-  .text
-  .global longest_inc_subseq
+.text
+.global longest_inc_subseq
 
 longest_inc_subseq:
-    mov x3, x2
-    cbz x3, return_zero
+    cmp x2, 0
+    beq return_zero
 
+    mov x3, 1
     mov x4, 0
-init_loop:
-    cmp x4, x3
-    bge fill_done
-    mov x5, 1
-    str x5, [x1, x4, lsl #3]
-    add x4, x4, 1
-    b init_loop
-fill_done:
 
-    mov x6, 1
-    mov x4, 1
 outer_loop:
-    cmp x4, x3
-    bge outer_done
-    mov x7, 0
-inner_loop:
-    cmp x7, x4
-    bge inner_done
-    ldr x8, [x0, x7, lsl #3]
-    ldr x9, [x0, x4, lsl #3]
-    cmp x8, x9
-    ble inner_next
-    ldr x10, [x1, x4, lsl #3]
-    ldr x11, [x1, x7, lsl #3]
-    add x11, x11, 1
-    cmp x10, x11
-    csel x10, x11, x10, gt
-    str x10, [x1, x4, lsl #3]
-inner_next:
-    add x7, x7, 1
-    b inner_loop
-inner_done:
+    cmp x4, x2
+    bge exit_outer_loop
 
-    ldr x10, [x1, x4, lsl #3]
-    cmp x6, x10
-    csel x6, x10, x6, gt
+    mov x5, 0
+    str wzr, [x1, x4, lsl #3]
+
+inner_loop:
+    cmp x5, x4
+    bge exit_inner_loop
+
+    ldr x6, [x0, x4, lsl #3]
+    ldr x7, [x0, x5, lsl #3]
+    cmp x6, x7
+    ble skip_assignment
+
+    ldr x8, [x1, x5, lsl #3]
+    add x8, x8, 1
+    ldr x9, [x1, x4, lsl #3]
+    cmp x8, x9
+    ble skip_assignment
+
+    str x8, [x1, x4, lsl #3]
+    cmp x3, x8
+    bge skip_assignment
+    mov x3, x8
+
+skip_assignment:
+    add x5, x5, 1
+    b inner_loop
+
+exit_inner_loop:
     add x4, x4, 1
     b outer_loop
-outer_done:
+
+exit_outer_loop:
+    mov x0, x3
+    ret
 
 return_zero:
     mov x0, 0
-    ret
-
-return_result:
-    mov x0, x6
     ret
 
